8829649fc60941778941aa2b784bc25b81c5e26d
Nnamani John <nnamanijohn123@gmail.com>
2023-04-12 00:36:42 +0100

    add and configure docker

---
+++ docker/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /var/www
+
+npm install
+npm run build
+
+echo "Running composer"
+composer install --no-dev
+
+php artisan migrate:fresh --seed
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+
+/usr/bin/supervisord -c /etc/supervisord.conf
